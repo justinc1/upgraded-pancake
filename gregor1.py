7811379272587831861d524bbacd8193a39dd7e2
justinc1 <justin.cinkelj@xlab.si>
2017-11-20 19:45:27 +0100

    Create gregor1.py
    added gregor1.py

---
+++ gregor1.py
@@ -0,0 +1,19 @@
+import sys
+
+data1 = input("Enter a number: ")
+data1 = int(data1)
+print ("data1 = %d" % data1)
+
+# print (data1 + 99)
+
+data2 = input("Enter a number: ")
+data2 = int(data2)
+print ("data2 = %d" % data2)
+
+print ("%d + %d = %d" % (data1, data2, data1 + data2) )
+
+print ("%d * %d = %d" % (data1, data2, data1 * data2) )
+
+print ("%d - %d = %d" % (data1, data2, data1 - data2) )
+
+print ("%d / %d = %d" % (data1, data2, data1 / data2) )
